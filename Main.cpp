2a3d74edcd8530889256653aaffdfccc0118b266
cons.domenico.landri <cons.domenico.landricina@cy4gate.com>
2022-09-05 14:27:51 +0200

    assets

--- Main.cpp
+++ Main.cpp
@@ -1,33 +1,40 @@
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 #include "Engine/include/Engine.hpp"
 #include <iostream>
-
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 720;
-
-int main(int argc, char* args[]);
-
-int WinMain(int argc, char* args[]){
-    return main(argc, args);
-}
 
 int main(int argc, char* args[])
 {
     SDL_Event e{};
     bool quit = false;
-    GFXManager::Init("Chironi",SCREEN_WIDTH,SCREEN_HEIGHT);
+    GFXManager::Init("Chironi");
+    GFXManager::DefaultFont = TTF_OpenFont( "Assets/Roboto-Black.ttf", 72 );
 
-    GFXManager::Add("Hello", "Assets/preview.png");
+    GFXManager::Add("Hello", "Assets/previewd.png");
     GFXManager::Add("Chiron", "Assets/Chironi.bmp");
+    GFXManager::Add("Saul", "Assets/sas.png");
+    GFXManager::Add("prev", "Assets/previewd.png");
+    GFXManager::Add("testo", GFXManager::CreateTextImage("Testo di prova", {255,255,255,255}));
 
-    Player player {150,150,"Chiron",Vector2{30,30},6};
-    GameObject obj {1280,100,"Hello",Vector2{0,620}};
-    GameObject obj2 {150,150,"Hello",Vector2{0,470}};
-    GameObject obj3 {300,300,"Hello",Vector2{400,70}};
+    Camera camera {"Main"};
+    Camera camera2 {"Gui"};
+
+    camera2.CameraRender.x = 0;
+    camera2.CameraRender.y = 0;
+    
+    Player player {"Chiron", 150,150, Vector2{30, 30}, 6};
+    GameObject obj {"Hello", 1280,100, Vector2{0, 620}};
+    GameObject obj2 {"Hello", 150,150, Vector2{0, 470}};
+    GameObject obj3 {"Hello", 150,150, Vector2{150, 320}};
+
+    GameObject obj4 {"testo", 0, 0, Vector2{0, 0}};
+    obj4.rb.DestroyCollider();
+
+    CameraManager::Get("Gui")->SetTarget(&obj4);
 
     std::string Title;
-    SDL_Rect viewPort = { SCREEN_WIDTH - 64 - 128 , 64, 128, 128 };
+    SDL_Rect viewPort = { GFXManager::LogicWidth - 64 - 128 , 64, 128, 128 };
 
     while (!quit) {
         //Clear screen
@@ -37,7 +44,7 @@ int main(int argc, char* args[])
         Clock::StartTick();
 
         while(SDL_PollEvent(&e) != 0){
-             if (e.type == SDL_QUIT)
+            if (e.type == SDL_QUIT)
             {
                 quit = true;
             }
@@ -48,13 +55,16 @@ int main(int argc, char* args[])
         UpdateManager::Update();
         
         PhysicsManager::Update();
+
+        CameraManager::Update();
         
+        //SDL_RenderCopy(GFXManager::Renderer, GFXManager::Get("Saul"), &camera.CameraRender, nullptr);
         DrawManager::Draw();
 
         PhysicsManager::Draw();
 
         SDL_RenderSetViewport(GFXManager::Renderer, &viewPort);
-        SDL_RenderCopy(GFXManager::Renderer, GFXManager::Get("Hello"), nullptr, nullptr);
+        SDL_RenderCopy(GFXManager::Renderer, GFXManager::Get("prev"), nullptr, nullptr);
         
         //Update screen
         SDL_RenderPresent( GFXManager::Renderer );
@@ -72,3 +82,8 @@ int main(int argc, char* args[])
     GFXManager::Close();
     return 0;
 }
+
+int WinMain(int argc, char* args[]){
+    return main(argc, args);
+}
+
